30d40a54ab3cf539e11c448eaf4d691c5d944a6a
smtaha512 <s.m.taha10@gmail.com>
2025-02-21 10:17:47 +0100

    feat: update data in the template and make print ready

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
@@ -26,4 +31,48 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    addPrintOnlyUrls();
 });
+
+function addPrintOnlyUrls() {
+
+    function beforePrint() {
+        const contactDetailsContainer = document.querySelector('#contact-details');
+        const printOnlyUrlsContainer = document.createElement('span');
+        printOnlyUrlsContainer.id = 'print-only-urls-container';
+        printOnlyUrlsContainer.classList = "d-none d-print-inline";
+        printOnlyUrlsContainer.innerHTML = `| <a href="https://www.smtaha.dev/linkedin" target="_blank">smtaha.dev/linkedin</a>
+            | <a href="https://www.smtaha.dev/github" target="_blank">smtaha.dev/github</a>
+            | <a href="https://www.smtaha.dev/medium" target="_blank">smtaha.dev/medium</a>
+        `;
+        contactDetailsContainer.appendChild(printOnlyUrlsContainer);
+    };
+
+    function afterPrint() {
+        const contactDetailsContainer = document.querySelector('#contact-details');
+        const printOnlyUrlsContainer = contactDetailsContainer.querySelector('#print-only-urls-container');
+
+        if (printOnlyUrlsContainer) {
+            contactDetailsContainer.removeChild(printOnlyUrlsContainer);
+        }
+    }
+
+
+    if (window.matchMedia) {
+        var mediaQueryList = window.matchMedia('print');
+        window.addEventListener(mediaQueryList, function (mql) {
+            if (mql.matches) {
+                beforePrint();
+            } else {
+                afterPrint();
+            }
+        })
+    }
+
+    window.onbeforeprint = beforePrint;
+    window.onafterprint = afterPrint;
+
+};
